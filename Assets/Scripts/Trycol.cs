e93f3376046d803200355ae3be54fade42b6ca07
FXXXXXXXK <122261943+FXXXXXXXK@users.noreply.github.com>
2024-10-25 15:21:38 +0800

    Trycol

--- Assets/Scripts/Trycol.cs
+++ Assets/Scripts/Trycol.cs
@@ -7,13 +7,13 @@ using UnityEngine;
 public class Trycol : MonoBehaviour
 {
     private bool isTrigger = true;
-    public  DialogueTreeController _dialogueTreeController;
+    public DialogueTreeController _dialogueTreeController;//获取对话树（就那个预制体）
     public void OnTriggerEnter(Collider other)
     {
         Debug.Log("1");
         if (other.CompareTag("Player") && isTrigger)
         {
-            _dialogueTreeController.StartDialogue();
+            _dialogueTreeController.StartDialogue();//启动对话
             isTrigger = false;
         }
         
